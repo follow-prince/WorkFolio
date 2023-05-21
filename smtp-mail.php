b7a00d104ea650fde43db79b14fb786c35949b7c
Prince <elavarasa.003@gmail.com>
2023-05-21 13:35:31 +0530

    update this file

--- smtp-mail.php
+++ smtp-mail.php
@@ -1,23 +1,34 @@
 <?php
-// SMTP server settings
-$smtp_server = 'smtp.example.com';
-$smtp_username = 'user@example.com';
-$smtp_password = 'password';
-$smtp_port = 587;
+use PHPMailer\PHPMailer\PHPMailer;
 
-// Get form data
+// Include the PHPMailer library
+require 'vendor/autoload.php';
+
+// Retrieve form data
 $fullname = $_POST['fullname'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// Send email
-$to = 'recipient@example.com';
-$subject = 'New message from contact form';
-$body = "Name: $fullname\n\nEmail: $email\n\nMessage:\n$message";
-$headers = "From: $email\r\n";
-if (mail($to, $subject, $body, $headers)) {
-  echo 'Message sent successfully';
+
+// Configure SMTP settings
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';  // Replace with your SMTP server host
+$mail->Port = 587;  // Replace with your SMTP server port
+$mail->SMTPAuth = true;
+$mail->Username = 'elavarasa.003@gmail.com';  // Replace with your SMTP username (email address)
+$mail->Password = 'zitifjqhotiairhf';  // Replace with your SMTP password
+
+// Set email details
+$mail->setFrom('elavarasa.003@gmail.com', 'Prince');  // Replace with the sender's email address and name
+$mail->addAddress('elavarasa.003@gmail.com');  // Replace with the recipient's email address
+$mail->Subject = 'New Contact Form Submission';
+$mail->Body = "Full Name: $fullname\nEmail: $email\nMessage: $message";
+
+// Send the email
+if (!$mail->send()) {
+    echo 'Error: ' . $mail->ErrorInfo;
 } else {
-  echo 'Message failed to send';
+    header('Location: index.php');
 }
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/autoload.php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e6a2b733478269c14daa2b8965fdb7b6910c7d41',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor/autoload.php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e6a2b733478269c14daa2b8965fdb7b6910c7d41',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
