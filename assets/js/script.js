6fbad552cbed904a000579d87b4f972fc26333a2
prince <elavarasa.003@gmail.com>
2023-07-17 11:31:48 +0530

    my details change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
 'use strict';
 
-
-
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function(elem) {
+  elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function() {
+  elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -28,60 +26,53 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const testimonialsModalFunc = function() {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
-  testimonialsItem[i].addEventListener("click", function () {
-
+  testimonialsItem[i].addEventListener("click", function() {
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
     testimonialsModalFunc();
-
   });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-
-
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
+const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+// toggle select dropdown on click
+select.addEventListener("click", function() {
+  elementToggleFunc(this);
+});
 
-// add event in all select items
+// add event to all select items
 for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
+  selectItems[i].addEventListener("click", function() {
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
-const filterFunc = function (selectedValue) {
-
+// filter function
+const filterFunc = function(selectedValue) {
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -89,87 +80,73 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
+};
 
-}
-
-// add event in all filter button items for large screen
+// add event to all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
+  filterBtn[i].addEventListener("click", function() {
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
-
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// add event to all form input fields
 for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
+  formInputs[i].addEventListener("input", function() {
     // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
-
   });
 }
 
-
-
-// page navigation variables
+// Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// Add event listener to all navigation links
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    for (let j = 0; j < pages.length; j++) {
+      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
-
   });
 }
 
-
 // _____________________________________________________________________
+
 var textElement = document.getElementById("text");
-var text = `\nI am deeply dedicated to transforming my love for web development into a fulfilling career. With an insatiable thirst for knowledge and a penchant for generating innovative ideas, I find great joy in continuously expanding my expertise. Spending ample time exploring the depths of the internet has endowed me with valuable open-source skills.\n
-Through this website, I aim to showcase my projects, skills, and experiences in web development. I believe in creating efficient and visually appealing websites that not only captivate users but also provide them with a seamless and enjoyable experience. From front-end design to back-end development, I strive to deliver high-quality solutions that meet the unique needs of each project.\n
-Whether you're a potential client, fellow developer, or someone simply interested in the world of web development, I invite you to explore my portfolio. Here, you'll find a collection of my work, demonstrating my proficiency in various programming languages, frameworks, and technologies. I'm always open to collaboration and new opportunities, so feel free to reach out if you'd like to discuss a project or connect with me.\n
+var text = `I am deeply dedicated to transforming my love for web development into a fulfilling career. With an insatiable thirst for knowledge and a penchant for generating innovative ideas, I find great joy in continuously expanding my expertise. Spending ample time exploring the depths of the internet has endowed me with valuable open-source skills.
+
+Through this website, I aim to showcase my projects, skills, and experiences in web development. I believe in creating efficient and visually appealing websites that not only captivate users but also provide them with a seamless and enjoyable experience. From front-end design to back-end development, I strive to deliver high-quality solutions that meet the unique needs of each project.
+
+Whether you're a potential client, fellow developer, or someone simply interested in the world of web development, I invite you to explore my portfolio. Here, you'll find a collection of my work, demonstrating my proficiency in various programming languages, frameworks, and technologies. I'm always open to collaboration and new opportunities, so feel free to reach out if you'd like to discuss a project or connect with me.
+
 Thank you for visiting, and I hope you enjoy exploring my portfolio as much as I enjoyed creating it. Let's build remarkable digital experiences together!`;
 
 // Exporting to HTML
 var htmlText = text.replace(/\n/g, "<br>");
-
-
 
 function showMore() {
   var readMoreButton = document.createElement("button");
@@ -182,23 +159,9 @@ function showMore() {
 function expandText() {
   textElement.innerHTML = htmlText;
   voiceElement.textContent = text; // Set the entire text for voice playback
-
 }
 
 showMore();
- // Start typing immediately
 
-
-
-
-
-
-
-
-
-
-
-
-//  ________________________________________________
 
 
