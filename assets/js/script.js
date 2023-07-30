87aa88ee4210b1e83e3439cadd8dbecaa876696c
Elavarasan <elavarasa.003@gmail.com>
2023-07-30 23:40:02 +0530

    just update

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,3 +173,29 @@ function openVideoPopup() {
 
 // Add click event listener to the link
 document.getElementById("videoLink").addEventListener("click", openVideoPopup);
+
+
+
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
