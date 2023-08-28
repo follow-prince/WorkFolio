3077d7a434092f69493bc4c6dfa036a911a4538d
ELAVARASAN <105858216+follow-prince@users.noreply.github.com>
2023-08-28 16:05:15 +0530

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+
 
   :focus {
     outline: 0;
@@ -13,14 +9,10 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: ${props => props.theme['base-background']};
     color: ${props => props.theme['base-text']}; 
     -webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button {
-    font: 400 1rem Nunito, 'sans-serif';
-  }
   
     /* width */
 ::-webkit-scrollbar {
